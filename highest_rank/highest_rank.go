37de13c6a403d0ca7ba831e9992c21bf36660df5
Stefan Boos <stefan.boos@gmx.de>
2021-06-15 05:55:15 +0200

    feat(highest_rank): add alternative solution using a map [int]int

--- highest_rank/highest_rank.go
+++ highest_rank/highest_rank.go
@@ -2,6 +2,25 @@ package highest_rank
 
 import "sort"
 
+func AlternativeSolution(nums []int) int {
+	frequencyByNumber := map[int]int{}
+
+	for _, num := range nums {
+		frequencyByNumber[num]++
+	}
+
+	highestFrequency := 0
+	mostFrequentNumber := 0
+	for num, freq := range frequencyByNumber {
+		if (freq > highestFrequency) || (freq == highestFrequency && num > mostFrequentNumber) {
+			highestFrequency = freq
+			mostFrequentNumber = num
+		}
+	}
+
+	return mostFrequentNumber
+}
+
 func HighestRank(nums []int) int {
 	sort.Ints(nums)
 

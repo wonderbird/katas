2e689b57304de0c9eab6079b5ef030e0a7e0a183
Stefan Boos <stefan.boos@gmx.de>
2021-06-15 05:43:48 +0200

    feat(highest_rank): implement kata using TDD / BDD

--- highest_rank/highest_rank.go
+++ highest_rank/highest_rank.go
@@ -1,5 +1,28 @@
 package highest_rank
 
+import "sort"
+
 func HighestRank(nums []int) int {
-	return nums[len(nums)-1]
+	sort.Ints(nums)
+
+	mostFrequentNumber := 0
+	highestFrequency := 0
+	currentNumber := 0
+	currentFrequency := 0
+
+	for _, num := range nums {
+		if num != currentNumber {
+			currentNumber = num
+			currentFrequency = 1
+		} else {
+			currentFrequency++
+		}
+
+		if currentFrequency >= highestFrequency {
+			mostFrequentNumber = num
+			highestFrequency = currentFrequency
+		}
+	}
+
+	return mostFrequentNumber
 }

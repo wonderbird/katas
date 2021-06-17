f7891385a09a2bd7574745de9220e6cc7db7a1d9
Stefan Boos <stefan.boos@gmx.de>
2021-06-17 06:11:11 +0200

    feat(playing_with_digits): implement maths for the task

--- playing_with_digits/dig_pow.go
+++ playing_with_digits/dig_pow.go
@@ -1,5 +1,32 @@
 package playing_with_digits
 
+import "math"
+
 func DigPow(n, p int) int {
-	return 0
+	// TODO can we leave this check out?
+	if n == 0 {
+		return -1
+	}
+
+	nf := float64(n)
+
+	sum := 0.0
+	numDigits := 1 + int(math.Log10(nf))
+	remainder := nf
+	for i := 0; i < numDigits; i++ {
+		currentDigitPosition := float64(numDigits - 1 - i)
+		currentDigitPow10 := math.Pow(10, currentDigitPosition)
+
+		currentDigitValue := math.Floor(remainder / currentDigitPow10)
+		sum += math.Pow(currentDigitValue, float64(p+i))
+
+		remainder -= currentDigitValue * currentDigitPow10
+	}
+
+	k := sum / float64(n)
+	if math.Trunc(k) == k {
+		return int(k)
+	}
+
+	return -1
 }
--- playing_with_digits/dig_pow_test.go
+++ playing_with_digits/dig_pow_test.go
@@ -2,6 +2,7 @@ package playing_with_digits_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 	"highest_rank/playing_with_digits"
 )
@@ -11,10 +12,37 @@ func dotest(n, p int, exp int) {
 	Expect(ans).To(Equal(exp))
 }
 
-var _ = Describe("Test Example", func() {
+var _ = Describe("Test DigPow", func() {
 
-	It("should handle basic cases", func() {
-		dotest(89, 1, 1)
+	DescribeTable("Inconclusive cases using n = 0",
+		dotest,
+		FEntry("0, 0 => -1 (undefined: 0^0)", 0, 0, -1),
+		Entry("0, 1 => -1 (inconclusive: 0^1 = 0 = 1 * 0 = ... * 0)", 0, 1, -1),
+		Entry("0, 5 => -1 (inconclusive: 0^5 = 0 = 1 * 0 = ... * 0)", 0, 5, -1),
+	)
+
+	DescribeTable("Cases using p = 0",
+		dotest,
+		Entry("1, 0 => 1  (1^0 = 1  = 1*1)", 1, 0, 1),
+		Entry("3, 0 => -1 (3^0 = 1 != k*3)", 3, 0, -1),
+	)
+
+	DescribeTable("Single digit cases using p = 1",
+		dotest,
+		Entry("1, 1 => 1  (1^1 = 1 = 1*1)", 1, 1, 1),
+		Entry("5, 1 => 1  (5^1 = 5 = 1*5)", 5, 1, 1),
+	)
+
+	DescribeTable("Double digit cases using p = 1",
+		dotest,
+		Entry("10, 1 => -1  (1^1 + 0^2 = 1 < 1*10)", 10, 1, -1),
+		Entry("11, 1 => -1  (1^1 + 1^2 = 2 < 1*11)", 11, 1, -1),
+		Entry("89, 1 => 1  (8^1 + 9^2 = 89 < 1*89)", 89, 1, 1),
+	)
+
+	PIt("(codewars) should handle basic cases", func() {
 		dotest(92, 1, -1)
+		dotest(695, 2, 2)
+		dotest(46288, 3, 51)
 	})
 })

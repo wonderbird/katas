717f6f77fc9e34bff23f4603f709d348251b4c3a
Stefan Boos <stefan.boos@gmx.de>
2021-06-17 05:08:31 +0200

    test(playing_with_digits): scaffold project

---
+++ playing_with_digits/dig_pow.go
@@ -0,0 +1,5 @@
+package playing_with_digits
+
+func DigPow(n, p int) int {
+	return 0
+}
---
+++ playing_with_digits/dig_pow_test.go
@@ -0,0 +1,20 @@
+package playing_with_digits_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/wonderbird/katas/playing_with_digits"
+)
+
+func dotest(n, p int, exp int) {
+	var ans = playing_with_digits.DigPow(n, p)
+	Expect(ans).To(Equal(exp))
+}
+
+var _ = Describe("Test Example", func() {
+
+	It("should handle basic cases", func() {
+		dotest(89, 1, 1)
+		dotest(92, 1, -1)
+	})
+})

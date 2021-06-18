0f70e34b4bf92c33eabe1848613998ee275ce6eb
Stefan Boos <stefan.boos@gmx.de>
2021-06-18 06:08:44 +0200

    feat(decode_morse_code_1): implement "Decode the Morse Code" kata

---
+++ decode_morse_code_1/decode_morse.go
@@ -0,0 +1,60 @@
+package decode_morse_code_1
+
+import "strings"
+
+func DecodeMorse(morseCode string) string {
+	morseCode = strings.TrimSpace(morseCode)
+
+	encodedWords := strings.Split(morseCode, "   ")
+	var decodedWords []string
+
+	for _, encodedWord := range encodedWords {
+		decodedWords = append(decodedWords, DecodeWord(encodedWord))
+	}
+	sentence := strings.Join(decodedWords, " ")
+
+	return sentence
+}
+
+func DecodeWord(morseCode string) string {
+	decoded := ""
+
+	splitCodes := strings.Split(morseCode, " ")
+	for _, code := range splitCodes {
+		decoded += MorseCodes[code]
+	}
+
+	return decoded
+}
+
+var MorseCodes = map[string]string{
+	".-":        "A",
+	"-...":      "B",
+	"-.-.":      "C",
+	"-..":       "D",
+	".":         "E",
+	"..-.":      "F",
+	"--.":       "G",
+	"....":      "H",
+	"..":        "I",
+	".---":      "J",
+	"-.-":       "K",
+	".-..":      "L",
+	"--":        "M",
+	"-.":        "N",
+	"---":       "O",
+	".--.":      "P",
+	"--.-":      "Q",
+	".-.":       "R",
+	"...":       "S",
+	"-":         "T",
+	"..-":       "U",
+	"...-":      "V",
+	".--":       "W",
+	"-..-":      "X",
+	"-.--":      "Y",
+	"--..":      "Z",
+	".-.-.-":    ".",
+	"-.-.--":    "!",
+	"...---...": "SOS",
+}
---
+++ decode_morse_code_1/decode_morse_code_1_suite_test.go
@@ -0,0 +1,13 @@
+package decode_morse_code_1_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestDecodeMorseCode1(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "DecodeMorseCode1 Suite")
+}

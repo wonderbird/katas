6e0b685b9a81a786cc3b8f1ead1b5821fe8fecfd
Stefan Boos <stefan.boos@gmx.de>
2021-06-18 22:15:32 +0200

    feat(decode_morse_code_2): (wip) decode bits to codewords

--- decode_morse_code_2/decode_bits.go
+++ decode_morse_code_2/decode_bits.go
@@ -1,11 +1,56 @@
 package decode_morse_code_2
 
+import (
+	"github.com/wonderbird/katas/decode_morse_code_1"
+)
+
 // You may get original char by morse code like this: MORSE_CODE[char]
+type CodeLengthPair struct {
+	Code   rune
+	Length int
+}
 
 func DecodeBits(bits string) string {
-	return ""
+	currentCode := '0'
+	currentLength := 0
+	codeLengths := []CodeLengthPair{}
+
+	for _, bit := range bits {
+		if bit == currentCode {
+			currentLength++
+		} else {
+			codeLengths = append(codeLengths, CodeLengthPair{
+				currentCode,
+				currentLength,
+			})
+			currentCode = bit
+			currentLength = 1
+		}
+	}
+	codeLengths = append(codeLengths, CodeLengthPair{
+		currentCode,
+		currentLength,
+	})
+
+	if codeLengths[0].Code == '0' {
+		codeLengths = codeLengths[1:]
+	}
+	timeUnit := codeLengths[0].Length
+
+	morseCode := ""
+	for _, codeLength := range codeLengths {
+		if codeLength.Code == '1' {
+			if codeLength.Length/timeUnit == 1 {
+				morseCode += "."
+			} else {
+				morseCode += "-"
+			}
+		}
+	}
+
+	return morseCode
 }
 
 func DecodeMorse(morseCode string) string {
-	return ""
+	return decode_morse_code_1.DecodeMorse(morseCode)
 }

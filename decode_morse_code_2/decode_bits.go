d3873189c6ccf4bb17823fae4ba646f4c19a5cb4
Stefan Boos <kontakt@boos.systems>
2021-06-21 05:51:54 +0200

    feat(decode_morse_code_2): (wip) decode bits to codewords

--- decode_morse_code_2/decode_bits.go
+++ decode_morse_code_2/decode_bits.go
@@ -2,6 +2,7 @@ package decode_morse_code_2
 
 import (
 	"github.com/wonderbird/katas-in-go/decode_morse_code_1"
+	"math"
 )
 
 // You may get original char by morse code like this: MORSE_CODE[char]
@@ -11,9 +12,18 @@ type CodeLengthPair struct {
 }
 
 func DecodeBits(bits string) string {
+	codeLengths := convertBitsToCodeLengthPairs(bits)
+	codeLengths = trimZeros(codeLengths)
+
+	timeUnit := findLengthOfShortestZeroBurst(codeLengths)
+	morseCode := convertCodeLengthPairsToMorseCode(codeLengths, timeUnit)
+
+	return morseCode
+}
+
+func convertBitsToCodeLengthPairs(bits string) (codeLengths []CodeLengthPair) {
 	currentCode := '0'
 	currentLength := 0
-	codeLengths := []CodeLengthPair{}
 
 	for _, bit := range bits {
 		if bit == currentCode {
@@ -32,11 +42,30 @@ func DecodeBits(bits string) string {
 		currentLength,
 	})
 
+	return
+}
+
+func trimZeros(codeLengths []CodeLengthPair) []CodeLengthPair {
 	if codeLengths[0].Code == '0' {
 		codeLengths = codeLengths[1:]
 	}
-	timeUnit := codeLengths[0].Length
 
+	numCodeWords := len(codeLengths)
+	if codeLengths[numCodeWords-1].Code == '0' {
+		codeLengths = codeLengths[:numCodeWords-1]
+	}
+	return codeLengths
+}
+
+func findLengthOfShortestZeroBurst(codeLengths []CodeLengthPair) int {
+	timeUnit := codeLengths[0].Length
+	for _, codeLength := range codeLengths {
+		timeUnit = int(math.Min(float64(timeUnit), float64(codeLength.Length)))
+	}
+	return timeUnit
+}
+
+func convertCodeLengthPairsToMorseCode(codeLengths []CodeLengthPair, timeUnit int) string {
 	morseCode := ""
 	for _, codeLength := range codeLengths {
 		if codeLength.Code == '1' {
@@ -45,9 +74,14 @@ func DecodeBits(bits string) string {
 			} else {
 				morseCode += "-"
 			}
+		} else if codeLength.Code == '0' {
+			if codeLength.Length/timeUnit == 3 {
+				morseCode += " "
+			} else if codeLength.Length/timeUnit == 7 {
+				morseCode += "   "
+			}
 		}
 	}
-
 	return morseCode
 }
 
